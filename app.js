93bbaf062cf11515f099ba28771929e156dae44d
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-05-06 23:19:05 +0530

    adding some styling (bug: not correctly setting the bgcolor of selected option)

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// // app.js
 import getQuestions from "./question.js";
 
 const science = getQuestions("science");
@@ -27,28 +26,13 @@ const nextbtn = document.querySelector(".controls button");
   });
 }
 
-// function CreateStartButton(classname) {
-//   const button = document.querySelector(`${classname}`);
-//   button.addEventListener("click", (evt) => {
-//     switch (evt.target.getAttribute("data-topic")) {
-//       case "science":
-//         StartScienceQuiz();
-//         break;
-//       default:
-//         console.log("another quiz is played");
-//         break;
-//     }
-//   });
-// }
-
 const startbuttons = Array.from(
   document.querySelectorAll(".quiz-start-button")
 );
-console.log(startbuttons);
 
 startbuttons.forEach((button) => {
   button.addEventListener("click", (evt) => {
-    // console.log(evt.target.value);
+
     switch (evt.target.value) {
       case "science":
         StartQuiz(science);
@@ -78,10 +62,7 @@ startbuttons.forEach((button) => {
   });
 });
 
-// document.querySelector(".main-container").style.display = "none";
-
 function StartQuiz(topic) {
-  console.log("starting Science Quiz", topic);
 
   mainContainer.classList.add("hide");
   quizSection.classList.remove("hide");
@@ -90,7 +71,6 @@ function StartQuiz(topic) {
 
   ShowQuestion(topic, quizCount);
   nextbtn.addEventListener("click", () => {
-    console.log(quizCount);
     let selectedOption = document.querySelector(".option-button.selected");
 
     if (!selectedOption) {
@@ -98,29 +78,18 @@ function StartQuiz(topic) {
       return;
     }
     const selectedValue = selectedOption.value;
-
+    
     checkAnswer(topic, selectedValue, quizCount);
-
-    // NextQuestion(topic);
   });
 }
 
 function ShowQuestion(topic, index = 0) {
-  // console.log(topic[index].question);
   document.querySelector(".question-section h3").textContent =
     topic[index].question;
   const options = Array.from(document.querySelectorAll(".option-button"));
-  // console.log(options);
-
-  // deselecting previously selected option;
-
-  options.forEach((option) => {
-    option.classList.remove("selected");
-  });
 
   options.forEach((option, count) => {
     option.innerHTML = `<h4>${topic[index].options[count + 1]}</h4>`;
-    // option.value = `${topic[index].options[count + 1]}`;
   });
 
   options.forEach((option) => {
@@ -129,6 +98,8 @@ function ShowQuestion(topic, index = 0) {
       option.classList.add("selected");
     });
   });
+  
+  
 }
 
 function NextQuestion(topic) {
@@ -141,26 +112,28 @@ function NextQuestion(topic) {
 }
 
 function checkAnswer(topic, selected = "not get any input", index) {
-  const correctOption = document.querySelector(`.option-button[value="${topic[index].correct}"]`);
-  const selectedOption = document.querySelector(`.option-button[value="${selected}"]`);
+
+  let correctOption = document.querySelector(
+    `.option-button[value="${topic[index].correct}"]`
+  );
+  let selectedOption = document.querySelector(
+    `.option-button[value="${selected}"]`
+  );
 
   if (topic[index].correct == selected) {
     score++;
-    console.log("matched");
-    selectedOption.style.backgroundColor = 'green';
+    selectedOption.style.backgroundColor = "green";
     setTimeout(() => {
-      selectedOption.style.backgroundColor = 'initial';
+      selectedOption.style.backgroundColor = "initial";
       NextQuestion(topic);
     }, 2000);
   } else {
-    console.log("selected option is :", selected);
-    console.log("current score :", score);
-    selectedOption.style.backgroundColor = 'red';
-    correctOption.style.backgroundColor = 'green';
-    
+    selectedOption.style.backgroundColor = "red";
+    correctOption.style.backgroundColor = "green";
+
     setTimeout(() => {
-      selectedOption.style.backgroundColor = 'initial';
-      correctOption.style.backgroundColor = 'initial';
+      selectedOption.style.backgroundColor = "initial";
+      correctOption.style.backgroundColor = "initial";
       NextQuestion(topic);
     }, 2000);
   }
@@ -170,5 +143,4 @@ function ShowResult() {
   quizSection.classList.add("hide");
   resultSection.classList.remove("hide");
   document.querySelector(".score").textContent = `${score}/${quizCount + 1}`;
-  console.log("printing Result");
 }
